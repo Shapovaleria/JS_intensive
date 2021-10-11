91fb7d0aaf697985c85fb3f4134be6a30b16d67d
Valeriia <shapovalvv12@gmail.com>
2021-10-11 13:53:44 +0300

    homework 4

---
+++ hometask_4/tasks.js
@@ -0,0 +1,67 @@
+function concatStrings (str, separator) {
+  let allStrings = `${str}`
+  function makeConcatination(x) {
+    if (typeof x == 'string') {
+      if (separator !== undefined && typeof separator == 'string') {
+        allStrings += `${separator}${x}`
+        return makeConcatination
+      }
+      else {
+        allStrings += `${x}`;
+        return makeConcatination
+      }
+    }
+    else {
+      return allStrings
+    }
+  };
+  return makeConcatination
+}
+
+
+class Calculator {
+  constructor(x, y, ...args) {
+    if (!Number.isFinite(x) || !Number.isFinite(y) || args.length !== 0) {
+      throw new Error('Function works with 2 numbers only')
+    }
+    this.x = x,
+    this.y = y,
+    this.setX = this.setX.bind(this),
+    this.setY = this.setY.bind(this),
+    this.logSum = this.logSum.bind(this),
+    this.logMul = this.logMul.bind(this),
+    this.logSub = this.logSub.bind(this),
+    this.logDiv = this.logDiv.bind(this)
+  }
+
+  setX(num) {
+    if (!Number.isFinite(num)) {
+      throw new Error('Number is not valid')
+    }
+    this.x = num
+  };
+  setY(num) {
+    if (!Number.isFinite(num)) {
+      throw new Error('Number is not valid')
+    }
+    this.y = num
+  };
+  logSum() {
+    console.log(this.x + this.y)
+  };
+  logMul() {
+    console.log(this.x * this.y)
+  };
+  logSub() {
+    console.log(this.x - this.y)
+  };
+  logDiv() {
+    if (this.y === 0) {
+      throw new Error('You can not divide by zero')
+    }
+    console.log(this.x / this.y)
+  }
+}
+
+
+
